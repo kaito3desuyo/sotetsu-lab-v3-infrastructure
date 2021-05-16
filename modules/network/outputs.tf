8bda2abaa50fc186d66f945de20c293fde7ea9e8
kaito3desuyo <admin@seapolis.me>
2021-05-16 16:32:12 +0900

    微修正

--- modules/network/outputs.tf
+++ modules/network/outputs.tf
@@ -3,9 +3,9 @@ output "vpc_id" {
 }
 
 output "public_subnet_ids" {
-  value = [aws_subnet.subnet["public_01a"].id, aws_subnet.subnet["public_01c"].id]
+  value = [aws_subnet.subnet["public_01a"].id, aws_subnet.subnet["public_01c"].id, aws_subnet.subnet["public_01d"].id]
 }
 
 output "db_subnet_ids" {
-  value = [aws_subnet.subnet["db_01a"].id, aws_subnet.subnet["db_01c"].id]
+  value = [aws_subnet.subnet["db_01a"].id, aws_subnet.subnet["db_01c"].id, aws_subnet.subnet["db_01d"].id]
 }
