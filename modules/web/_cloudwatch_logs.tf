115a388f4b75b46bc1830bd51f70cdbc8f5d9729
kaito3desuyo <admin@seapolis.me>
2022-11-03 19:26:04 +0900

    refactor: :fire: webモジュールのCodePipelineを廃止

--- modules/web/_cloudwatch_logs.tf
+++
@@ -1,3 +0,0 @@
-resource "aws_cloudwatch_log_group" "for_web_codebuild" {
-  name = "/aws/codebuild/${var.name}-prod-web-codebuild"
-}
--- modules/web/_codestar_connections.tf
+++
@@ -1,4 +0,0 @@
-resource "aws_codestarconnections_connection" "for_web" {
-  name          = "${var.name}-web-csc"
-  provider_type = "GitHub"
-}
