618a263afe666f7ffb600a84cf058d973ef74b04
kaito3desuyo <admin@seapolis.me>
2023-10-31 21:10:06 +0900

    refactor: :wrench: パラメーターを調整する

--- modules/database/_rds.tf
+++ modules/database/_rds.tf
@@ -1,5 +1,6 @@
 locals {
-  rds_proxy_name = "${local.prefix}-rds-proxy"
+  rds_param_group_name = "${local.prefix}-rds-param-group"
+  rds_proxy_name       = "${local.prefix}-rds-proxy"
 }
 
 resource "aws_db_subnet_group" "main" {
@@ -10,32 +11,52 @@ resource "aws_db_subnet_group" "main" {
   }
 }
 
+resource "aws_db_parameter_group" "main" {
+  name   = local.rds_param_group_name
+  family = "postgres12"
+
+  parameter {
+    name         = "max_connections"
+    value        = "LEAST({DBInstanceClassMemory*2/9531392},5000)"
+    apply_method = "pending-reboot"
+  }
+
+  tags = merge(
+    local.module_tags,
+    {
+      "Name" = local.rds_param_group_name
+    }
+  )
+}
+
 resource "aws_db_instance" "main" {
   engine                          = "postgres"
   engine_version                  = "12.5"
   identifier                      = var.name
   username                        = var.main_db_username
   password                        = var.main_db_password
-  instance_class                  = "db.t4g.micro"
+  instance_class                  = "db.t4g.small"
   storage_type                    = "gp3"
-  storage_encrypted               = false
   allocated_storage               = 20
   max_allocated_storage           = 1000
-  availability_zone               = "ap-northeast-1a"
+  storage_encrypted               = false
   multi_az                        = false
+  availability_zone               = "ap-northeast-1a"
   db_subnet_group_name            = aws_db_subnet_group.main.name
   vpc_security_group_ids          = [aws_security_group.for_rds.id]
-  apply_immediately               = true
-  auto_minor_version_upgrade      = true
-  deletion_protection             = true
-  monitoring_interval             = 60
-  performance_insights_enabled    = true
-  enabled_cloudwatch_logs_exports = ["postgresql", "upgrade"]
+  parameter_group_name            = aws_db_parameter_group.main.name
   backup_retention_period         = 7
   backup_window                   = "17:00-18:00"
   copy_tags_to_snapshot           = true
+  maintenance_window              = "sun:18:00-sun:19:00"
+  auto_minor_version_upgrade      = true
+  monitoring_interval             = 60
+  performance_insights_enabled    = true
+  enabled_cloudwatch_logs_exports = ["postgresql", "upgrade"]
+  deletion_protection             = true
   skip_final_snapshot             = false
   final_snapshot_identifier       = "${var.name}-main-rds-final-snapshot"
+  apply_immediately               = true
 
   tags = {
     "Name" = "${var.name}-main-rds"
@@ -54,7 +75,7 @@ resource "aws_db_proxy" "main" {
   role_arn               = aws_iam_role.rds_proxy_role.arn
   vpc_subnet_ids         = var.subnet_ids
   vpc_security_group_ids = [aws_security_group.for_rds_proxy.id]
-  idle_client_timeout    = 1
+  idle_client_timeout    = 60
 
   auth {
     auth_scheme = "SECRETS"
@@ -67,8 +88,9 @@ resource "aws_db_proxy_default_target_group" "main" {
   db_proxy_name = aws_db_proxy.main.name
 
   connection_pool_config {
-    max_connections_percent      = 80
-    max_idle_connections_percent = 0
+    max_connections_percent      = 100
+    max_idle_connections_percent = 20
+    connection_borrow_timeout    = 20
   }
 }
 
